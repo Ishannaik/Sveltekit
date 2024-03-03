96de901872b8d2b0581021bbc0531da2ae999ab2
Ishan <ishannaik7@gmail.com>
2024-03-03 10:21:37 +0530

    No idea what I'm doing here

---
+++ myapp/src/routes/boat/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import type { PageData } from "./$types";
+
+  export let data: PageData;
+</script>
+
+<h1>{data.boatName}</h1>
+
+<form method="POST">
+  <input type="text" name="boatName" />
+  <button type="submit">Submit</button>
+</form>
