b0f4318d23dc22cac50be88c895a4589d96e30ed
Ishan <ishannaik7@gmail.com>
2024-03-17 11:54:04 +0530

    Two way binding in Svelte

--- Svelte.js - The Complete Guide/project-setup/src/App.svelte
+++ Svelte.js - The Complete Guide/project-setup/src/App.svelte
@@ -28,7 +28,8 @@
 <h1>Hello {uppercaseName}!, My age is {age}</h1>
 <button on:click={incrementAge}>Change Age</button>
 <!-- <button on:click={changeName}>Change Name</button> -->
-<input type="text" value={name} on:input={nameInput} />
+<!-- <input type="text" value={name} on:input={nameInput} /> -->
+<input type="text" bind:value="name"/>
 
 <style>
   h1 {
