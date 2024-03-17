bf39d0a2e527fbbd9c5258425806a09b14bb99ee
Ishan <ishannaik7@gmail.com>
2024-03-17 11:37:49 +0530

    Writing some svelte code

--- Svelte.js - The Complete Guide/project-setup/src/App.svelte
+++ Svelte.js - The Complete Guide/project-setup/src/App.svelte
@@ -1,11 +1,17 @@
 <script>
-	export let name;
+  export let name;
+  export let age;
+
+  function incrementAge() {
+    age += 1;
+  }
 </script>
 
-<style>
-	h1 {
-		color: purple;
-	}
-</style>
+<h1>Hello {name}!, My age is {age}</h1>
+<button on:click={incrementAge}>Change Age</button>
 
-<h1>Hello {name}!</h1>
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
