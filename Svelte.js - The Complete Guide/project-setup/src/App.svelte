48c1c08181b90b724fdf00694b245456b2c0e4e3
Ishan <ishannaik7@gmail.com>
2024-03-17 12:00:14 +0530

    Using multiple components in a Svelte project

--- Svelte.js - The Complete Guide/project-setup/src/App.svelte
+++ Svelte.js - The Complete Guide/project-setup/src/App.svelte
@@ -1,4 +1,5 @@
 <script>
+  import ContactCard from "./ContactCard.svelte";
   let name = "Ishan";
   let age = 20;
 
@@ -29,10 +30,12 @@
 <button on:click={incrementAge}>Change Age</button>
 <!-- <button on:click={changeName}>Change Name</button> -->
 <!-- <input type="text" value={name} on:input={nameInput} /> -->
-<input type="text" bind:value="name"/>
+<input type="text" bind:value={name} />
 
 <style>
   h1 {
     color: purple;
   }
 </style>
+
+<ContactCard />
