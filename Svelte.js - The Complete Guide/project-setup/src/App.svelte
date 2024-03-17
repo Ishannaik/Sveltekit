4303add73676f7e44e9bb2260324dcd4b6aea64c
Ishan <ishannaik7@gmail.com>
2024-03-17 11:51:01 +0530

    Learnt about Reactive variables

--- Svelte.js - The Complete Guide/project-setup/src/App.svelte
+++ Svelte.js - The Complete Guide/project-setup/src/App.svelte
@@ -1,14 +1,29 @@
 <script>
-  export let name;
-  export let age;
+  let name = "Ishan";
+  let age = 20;
+
+  $: uppercaseName = name.toUpperCase();
+
+  $: console.log(name);
+
+  $: if (name === "Ishan") {
+    console.log("Name is Ishan");
+  } else {
+    console.log("Name is not Ishan");
+  }
 
   function incrementAge() {
     age += 1;
   }
+
+  function changeName() {
+    name = "Ishan Naik";
+  }
 </script>
 
-<h1>Hello {name}!, My age is {age}</h1>
+<h1>Hello {uppercaseName}!, My age is {age}</h1>
 <button on:click={incrementAge}>Change Age</button>
+<button on:click={changeName}>Change Name</button>
 
 <style>
   h1 {
